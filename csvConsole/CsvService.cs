f21d2c788e374f3a268deb6dd77581e885cedaeb
Smax Laptop <home.maxi.schmidt@gmail.com>
2023-10-02 14:42:46 +0200

    added comments

--- csvConsole/CsvService.cs
+++ csvConsole/CsvService.cs
@@ -10,12 +10,14 @@ namespace csvConsole;
 
 public class CsvService
 {
+    /// <summary>
+    /// Writes the content of a .csv file to the console.
+    /// </summary>
     public void DisplayFile()
     {
         string path = "D:\\Coding\\EKE\\2APEC\\ITL-Singer\\CsvToolDev\\csvConsole\\test.csv";
         List<List<string>> rows = GetRows(path);
-        List<List<string>> columns = GetColumns(rows);
-        List<List<string>> unifiedRows = UnifyColumns(columns);
+        List<List<string>> unifiedRows = UnifyRows(rows);
 
         foreach (List<string> records in unifiedRows)
         {
@@ -27,6 +29,12 @@ public class CsvService
         }
     }
 
+    /// <summary>
+    /// Reads a .csv file and saves the content as single rows in a List<List<string>>.
+    /// It also removes the separator
+    /// </summary>
+    /// <param name="csvFilePath"></param>
+    /// <returns></returns>
     private static List<List<string>> GetRows(string csvFilePath)
     {
         List<List<string>> rows = new List<List<string>>();
@@ -41,6 +49,42 @@ public class CsvService
         return rows;
     }
 
+
+    /// <summary>
+    /// Adds empty spaces to the end of each element of a column until each element
+    /// has a length of the longest item from that column + 2 (to get a gap between columns).
+    /// </summary>
+    /// <param name="rows">All rows with all its elements</param>
+    /// <returns>all rows with all its elements having the same string length of the longest item + 2</returns>
+    public static List<List<string>> UnifyRows(List<List<string>> rows)
+    {
+        List<List<string>> columns = GetColumns(rows);
+
+        foreach (List<string> column in columns)
+        {
+            int longestItemLength = column.Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur).Length + 2; // add two more space for a little gap between columns
+
+            // loop through each column
+            for (int j = 0; j < column.Count; j++)
+            {
+                // loop through each item of column
+                for (int i = 0; i < column.Count; i++)
+                {
+                    while (column[i].Length <= longestItemLength)
+                    {
+                        column[i] += " ";
+                    }
+                }
+            }
+        }
+        return SwapInnerAndOuterList(columns);
+    }
+
+    /// <summary>
+    /// Gets the Columns from rows
+    /// </summary>
+    /// <param name="rows">List of all rows</param>
+    /// <returns>List of all columns</returns>
     private static List<List<string>> GetColumns(List<List<string>> rows)
     {
         int maxWords = rows.Max(record => record.Count);
@@ -66,29 +110,13 @@ public class CsvService
         return columns;
     }
 
-    public static List<List<string>> UnifyColumns(List<List<string>> columns)
-    {
-        foreach (List<string> column in columns)
-        {
-            int longestItemLength = column.Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur).Length + 2; // add two more space for a little gap between columns
-
-            // loop through each column
-            for (int j = 0; j < column.Count; j++)
-            {
-                // loop through each item of column
-                for (int i = 0; i < column.Count; i++)
-                {
-                    while (column[i].Length <= longestItemLength)
-                    {
-                        column[i] += " ";
-                    }
-                }
-            }
-        }
-        return GetUnifiedRows(columns);
-    }
-
-    private static List<List<string>> GetUnifiedRows(List<List<string>> columns)
+    /// <summary>
+    /// Basically swaps the inner List with the outer List. 
+    /// Pass the list of columns and get the list of rows
+    /// </summary>
+    /// <param name="columns">List of all columns with all strings of each column</param>
+    /// <returns>List of all rows with all strings for each row</returns>
+    private static List<List<string>> SwapInnerAndOuterList(List<List<string>> columns)
     {
         //basically swap inner and outer List
         int numRows = columns.Count;
